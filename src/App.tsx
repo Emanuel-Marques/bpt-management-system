073451066806449e2456b356d0b2ddf5d606fb92
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2024-10-21 10:09:02 +0100

    :recycle: refactor: remoção do header no componente app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import banner from "./assets/main-banner.jpg";
 import Footer from "./components/footer";
-import Header from "./components/header";
 import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header 
-      banner={banner} 
-      isMainPage={true}
-      />
-        <Outlet />
-      <Footer/>
+      <Outlet />
+      <Footer />
     </>
   )
 }
