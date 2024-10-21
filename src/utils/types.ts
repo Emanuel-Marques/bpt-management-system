4f3d1621cdc03a223f9bd79140455e0e15deda50
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2024-10-21 09:35:29 +0100

    :card_file_box: raw: tipo do card do atleta

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -7,3 +7,16 @@ export interface DropdownProps {
     title: string;
     items: itemDropdown[];
 }
+
+export interface HeaderProps {
+    banner: string;
+    isMainPage: boolean;
+    title?: string;
+}
+
+export interface AthleteCardProps {
+    name: string;
+    belt: string;
+    image: string;
+    description: string;
+}
