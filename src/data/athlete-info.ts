e50f53af0e68d71fb871c07e91fedd326691ea50
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2024-10-22 12:05:44 +0100

    :sparkles: feat: página de nossos atletas

--- src/data/athlete-info.ts
+++ src/data/athlete-info.ts
@@ -1,5 +1,8 @@
 import mestrao from "../assets/mestrao.jpg";
 import brett from "../assets/brett.jpg";
+import arilton from "../assets/athletes/arilton.jpg";
+import miguel from "../assets/athletes/miguel.jpg";
+import chox from "../assets/athletes/chox.jpg";
 export const athleteInfo = [
     {
         name: "Ricardo Marcelino",
@@ -14,5 +17,41 @@ export const athleteInfo = [
         image: brett,
         description: "Brett Carvalho é um faixa marrom de Jiu-Jitsu com 10 anos de experiência no esporte. Ele é um competidor de alto nível e um instrutor dedicado, conhecido por sua técnica apurada e sua abordagem centrada no aluno.",
         role: "Instrutor"
-    }
+    },
+    {
+        name: "Luís Somavie",
+        belt: "Roxa",
+        image: "https://placehold.co/366x489",
+        description: "Luís Somavie, faixa roxa com 9 anos de experiência em jiu-jitsu, começou a treinar em 2015 motivado por um impulso de seu pai. Desde então, o jiu-jitsu tem sido uma grande influência em sua vida, ajudando-o a desenvolver autoconfiança e disciplina.",
+        role: "Atleta"
+    },
+    {
+        name: "Luís Somavie",
+        belt: "Roxa",
+        image: "https://placehold.co/366x489",
+        description: "Luís Somavie, faixa roxa com 9 anos de experiência em jiu-jitsu, começou a treinar em 2015 motivado por um impulso de seu pai. Desde então, o jiu-jitsu tem sido uma grande influência em sua vida, ajudando-o a desenvolver autoconfiança e disciplina.",
+        role: "Atleta"
+    },
+    {
+        name: "Elísio Mateus",
+        belt: "Azul",
+        image: chox,
+        description: "Elísio Carlos Mateus, faixa azul com 3 anos de experiência em jiu-jitsu, foi inspirado pelos amigos da 'Tirito’s Campany🔥' a iniciar sua jornada no esporte. Desde então, o jiu-jitsu tem lhe proporcionado muitos benefícios, entre eles a confiança e a coragem para não ter medo de errar.",
+        role: "Atleta"
+    },
+    {
+        name: "Miguel Samulaia",
+        belt: "Azul",
+        image: miguel,
+        description: "Miguel Arcanjo Samulaia, faixa azul com 4 anos de experiência no jiu-jitsu, iniciou sua jornada em 2021 após ser convidado para treinar. Desde então, o jiu-jitsu transformou seu estilo de vida, trazendo disciplina e foco tanto dentro quanto fora dos tatames.eu maior objetivo é se tornar campeão e alcançar o topo do esporte.",
+        role: "Atleta"
+    },
+    {
+        name: "Arilton Félix",
+        belt: "Azul",
+        image: arilton,
+        description: "Arilton Félix, faixa azul com 2 anos e 3 meses de experiência em jiu-jitsu, começou a treinar em 2022, inspirado por amigos e seu irmão. Ao longo de sua jornada, o jiu-jitsu lhe trouxe diversos benefícios, como resiliência, persistência, humildade, além da capacidade de agir sob pressão e se superar diariamente.",
+        role: "Atleta"
+    },
+    
 ]
