09b0df970496389ff4534657218241148f7654db
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2024-10-28 00:36:46 +0100

    :sparkles: feat: Pagina de horário

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,7 @@ import App from "../App";
 import AboutOurAcademy from "../pages/about-our-academy";
 import MainPage from "../pages/main-page";
 import OurAthletes from "../pages/our-athletes";
+import Schedule from "../pages/schedule";
 
 export const routes = [
     {
@@ -9,6 +10,7 @@ export const routes = [
             { path: '/', element: <MainPage /> },
             { path: '/nossa-academia', element: <AboutOurAcademy /> },
             { path: '/nossos-atletas', element: <OurAthletes /> },
+            { path: '/horario-de-aulas', element: <Schedule /> },
         ]
     },
 ]
